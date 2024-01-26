bba154120725ac4981157d2660da4a5b8ea63057
Lucas-Paiv <jucaspaiva@gmail.com>
2024-01-26 12:31:24 -0300

    htmls para lista de livros e descricao deles

--- livro/urls.py
+++ livro/urls.py
@@ -3,6 +3,7 @@ from . import views #importando a pasta livro
 
 urlpatterns = [
 
-    path('home/', views.home, name='home')
+    path('home/', views.home, name='home'),
+    path('ver_livro/<int:id>', views.ver_livros, name='ver_livros')
 
 ]
